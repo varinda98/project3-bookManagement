599e7ae75b1dfa55f1de632e59f43b394811115b
Harsh-Kumar-Sharma <gaurharsh1737@gmail.com>
2022-11-28 10:27:10 +0530

    clear

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://SubhoYadav:2YqGHmy0GiXcwxCR@cluster0.ph4caby.mongodb.net/Project-1-Blog-Site", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+
+app.use('/', route);
+
+app.use((req,res)=>{
+    res.status(404).send("request not found")
+})
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
